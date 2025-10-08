3abe2bb02df230ef697c6193ae6d13a182c27115
Mega <megachentw0622@gmail.com>
2025-10-08 18:25:04 +0800

    寫完報告了

--- lab1/AG_5.v
+++ lab1/AG_5.v
@@ -1,7 +1,7 @@
-module AG_5(clk, Addr);
-input clk;
-output [4:0] Addr;
-reg [4:0] Addr;
-always @(posedge clk)
-	Addr <= Addr+1;
-endmodule
+module AG_5(clk, Addr);	//Define module
+input clk;					//Declare 1 bit input 'clk':
+output [4:0] Addr;		//Declare 5 bit output 'Addr':
+reg [4:0] Addr;			//Declare 'Addr' to be 'reg' type: default declaration is using 'wire'
+always @(posedge clk)	//Triggered by posedge of clk
+	Addr <= Addr+1;		//Non-blockingly assign 'Addr' to be 'Addr'+1
+endmodule					//End of this module
--- lab1/fDiv50MHzto1Hz.v
+++ lab1/fDiv50MHzto1Hz.v
@@ -1,15 +1,19 @@
 module fDiv50MHzto1Hz(fin, fout);
 input fin;
-output [4:0] fout;
+output fout;
 wire [31:0] DIVN, _DIVN;
 reg [31:0] count;
 reg fout;
-assign DIVN = 32'd50000000;
-assign _DIVN = {1'b0, DIVN[31:1]};
+assign DIVN = 32'd10;//32'd50000000;
+assign _DIVN = 32'd5;//{1'b0, DIVN[31:1]};
+
+initial begin
+	count <= 32'd1; //not included in the given pack. BAD HABBIT!!
+	end
 
 always @(posedge fin)
 	begin	
-		count <= (count >= DIVN)? 32'd1:count+1;
-		fout <= (count <= _DIVN)? 1'b0:1'b1;
+		count <= (count >= DIVN)? 32'd1: count+1;
+		fout <= (count <= _DIVN)? 1'b1 : 1'b0;
 	end
 endmodule
